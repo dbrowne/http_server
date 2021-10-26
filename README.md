6fc8c410a800009e479304617571f60460fbeba4
dbrowne <dwight@dwightjbrowne.com>
2021-10-25 21:19:04 -0600

    Working version

---
+++ README.md
@@ -0,0 +1,5 @@
+# TEMP README  10/26/21
+# Rocket http server
+
+# Quick and dirty code  Will be refined in subsequent commits
+
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,78 @@
+//! Simple Rocket HTTP server
+//! Dwight J. Browne
+//! Quick and dirty example
+//! -p port #
+//! Environment variables are
+//! CLIENT_PORT
+//! Hard coded localhost 127.0.0.1
+//! Not creating a dependency on the ROCKET_ environment vars.
+
+
 #[macro_use] extern crate rocket;
 extern crate chrono;
 
-use std::error::Error;
-use std::io;
 use chrono::offset::Utc;
 use chrono::DateTime;
 use std::time::{SystemTime};
+use git_version::git_version;
+use std::net::{IpAddr,Ipv4Addr};
+use structopt::StructOpt;
+use std::env;
+use rocket::Config;
 
+const GIT_VERSION: &str = git_version!();
 #[get("/")]
 fn index() -> String {
     let system_time = SystemTime::now();
     let datetime: DateTime<Utc> = system_time.into();
     let  host_name = match hostname::get(){
-        Err(er) => panic!("Can't retreive the system hostname: {}",er.description()),
+        Err(er) => panic!("Can't retrieve the system hostname: {}",er),
         Ok(host_name) =>host_name
     };
-
-    format!("{:?}: {}:", host_name, datetime.format("%m/%d/%Y: %T \n"))
+    format!("{:?}: {} UTC: version: ->{}<- \n", host_name, datetime.format("%m/%d/%Y: %T"), GIT_VERSION)
 }
 
 #[rocket::main]
 #[allow(unused_must_use)]
 async fn main() {
-    rocket::build().mount("/", routes![index]).launch().await;
+    const K_PORT: &str = "3333";
+    const K_ADDR: IpAddr = IpAddr::V4(Ipv4Addr::new(127,0,0,1)); //for future use
+
+    // check for command line args
+    #[derive(StructOpt, Debug)]
+    // #[derive(Debug)]
+    #[structopt(rename_all = "kebab-case")]
+    struct Opt {
+        #[structopt(default_value = K_PORT, short)]
+        port: u16,
+    }
+    let opt = Opt::from_args();
+
+    let _port: u16;
+    // check for environment variables
+    let env_port: String = env::var("SERVER_PORT").unwrap_or(K_PORT.to_string());
+
+    // see if we have any input args
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        println!("Have input args: Overriding default vars with the following: ENV VARS IGNORED!");
+        println!("{:#?}", &opt);
+        _port = opt.port;
+
+    }else {
+        println!("Using environment vars: with the following values");
+        println!("port: SERVER_PORT                        {}", env_port);
+        _port = env_port.parse::<u16>().unwrap();
+    }
+
+    let config = Config {
+        port: _port,
+        address: K_ADDR.into(),
+        ..Config::default()
+    };
+
+    rocket::custom(&config).mount("/", routes![index]).launch().await;
+
 }
 
 
